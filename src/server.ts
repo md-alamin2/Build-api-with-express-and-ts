8105db84523394db2e054309418331cdedfadcbe
md-alamin2 <mdalamin22671@gmail.com>
2026-09-24 01:15:02 +0600

    completed the other apis

--- src/server.ts
+++ src/server.ts
@@ -174,6 +174,37 @@ app.delete("/users/:id", async (req: Request, res: Response) => {
   }
 });
 
+// todo CRUD
+app.post("/todos", async (req: Request, res: Response) => {
+  const { user_id, title } = req.body;
+  try {
+    const result = await pool.query(
+      `INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING *`,
+      [user_id, title],
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "Todo added successfully",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// 404 route
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    path: req.path,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
